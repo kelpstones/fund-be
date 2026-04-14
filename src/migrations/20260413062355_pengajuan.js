c56f3c9a4b3b952b057da98bd2e6c4fbadeb4761
danendra <pr.endra48@gmail.com>
2026-04-14 21:27:41 +0700

    feat: initial pengajuan and approvals; fix: add bisnis limit

--- src/migrations/20260413062355_pengajuan.js
+++ src/migrations/20260413062355_pengajuan.js
@@ -2,21 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable("pengajuan", (table) => {
     table.increments("id").primary();
     table.integer("bisnis_id").unsigned().notNullable();
     table.foreign("bisnis_id").references("bisnis.id").onDelete("CASCADE");
-    table.enum("status", ["pending", "approved", "rejected"]).defaultTo("pending");
-    table.string("alasan_reject").nullable();
+    table.bigint("target_pendanaan").notNullable();
+    table.bigint("total_pendanaan").defaultTo(0);
+    table.bigint("per_anual_return").notNullable();
     table.timestamps(true, true);
-  })
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
---
+++ src/migrations/20260414141409_approvals.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("approvals", (table) => {
+    table.increments("id").primary();
+    table.integer("pengajuan_id").unsigned().notNullable();
+    table
+      .foreign("pengajuan_id")
+      .references("pengajuan.id")
+      .onDelete("CASCADE");
+    table.integer("approver_id").unsigned().notNullable();
+    table.foreign("approver_id").references("users.id").onDelete("CASCADE");
+    table
+      .enum("status", ["approved", "rejected", "pending", "ongoing"])
+      .notNullable();
+    table.text("catatan").nullable();
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
