85e95988164f844a6a7613572c5ab263dca198f9
danendra <pr.endra48@gmail.com>
2026-04-15 16:38:16 +0700

    feat: initiating negosiasi and log_negosiasi

---
+++ src/migrations/20260415075656_log_negosiasis.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("log_negosiasis", (table) => {
+    table.increments("id").primary();
+    table.integer("negosiasi_id").unsigned().notNullable();
+    table
+      .foreign("negosiasi_id")
+      .references("negosiasis.id")
+      .onDelete("CASCADE");
+    table.integer("pengirim_id").unsigned().notNullable();
+    table.foreign("pengirim_id").references("users.id").onDelete("CASCADE");
+    table.integer("penawaran_return").notNullable();
+    table.text("catatan").nullable();
+    table.timestamps(true, true);
+  });
+};
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
