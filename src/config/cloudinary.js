239ed0512554e00e456a0ae706de5b83e86bfab4
danendra <pr.endra48@gmail.com>
2026-05-18 19:05:07 +0700

    fix: depedencies error

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,5 +1,5 @@
 const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
 const multer = require("multer");
 
 cloudinary.config({
@@ -8,14 +8,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "fundraise/dokumen-bisnis",
-    allowed_formats: ["jpg", "jpeg", "png", "pdf"],
-    resource_type: "auto",
-  },
-});
+
 
 const upload = multer({
   storage: storage,
