cbf87dd49424c7343da61e65b59bb0eff24c9738
danendra <pr.endra48@gmail.com>
2026-05-18 15:06:42 +0700

    init: dokumen bisnis

--- src/migrations/20260518070952_dokumen_bisnis.js
+++ src/migrations/20260518070952_dokumen_bisnis.js
@@ -2,14 +2,32 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("dokumen_bisnis", (table) => {
+    table.increments("id").primary();
+    table.integer("bisnis_id").unsigned().notNullable();
+    table.foreign("bisnis_id").references("bisnis.id").onDelete("CASCADE");
+    table
+      .enum("jenis_dokumen", [
+        "legalitas_usaha",
+        "proposal_pendanaan",
+        "laporan_penjualan",
+      ])
+      .notNullable();
+    table.string("file_url").notNullable();
+    table
+      .enum("status", ["belum_upload", "pending", "valid", "invalid"])
+      .defaultTo("belum_upload")
+      .notNullable();
+    table.text("catatan").nullable();
+    table.timestamps(true, true);
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("dokumen_bisnis");
 };
