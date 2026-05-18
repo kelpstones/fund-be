0163916ecb1d59f5e355edcfbcc07c16fd573cf4
danendra <pr.endra48@gmail.com>
2026-05-18 18:55:06 +0700

    fix: add drop table bisnis cover

--- src/migrations/20260518083352_bisnis_cover.js
+++ src/migrations/20260518083352_bisnis_cover.js
@@ -17,4 +17,6 @@ exports.up = function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {};
+exports.down = function (knex) {
+    return knex.schema.dropTable("bisnis_cover");
+};
