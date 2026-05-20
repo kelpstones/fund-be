c60a213796ee39962b8a35dda81034158d868849
danendra <pr.endra48@gmail.com>
2026-05-20 16:07:43 +0700

    fix: knexfile .env

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+});
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
