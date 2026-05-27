67c89e224e98c116e4e8c358b368bcc2986d365b
danendra <pr.endra48@gmail.com>
2026-05-27 22:44:16 +0700

    fix: xendit callback

--- src/middlewares/key.js
+++ src/middlewares/key.js
@@ -1,8 +1,15 @@
 const responseHelper = require("../utils/index").ResponseHelper;
 const logger = require("../utils/index").logger;
 exports.validateApiKey = (req, res, next) => {
+  const isXenditCallback =
+    req.method === "POST" && req.path === "/wallet/xendit-callback";
+
+  if (isXenditCallback) {
+    return next();
+  }
+
   const ourKey = process.env.API_KEY;
-  const apiKey = req.headers["x-api-key"] || req.query.api_key;
+  const apiKey = req.headers["x-api-key"];
 
   if (!apiKey) {
     logger.warn("Missing API Key", {
